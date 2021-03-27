50280bedd4330e729f0ff875de354aaf2168da96
Andika <andikasaputra.id@gmail.com>
2021-03-28 00:56:26 +0700

    update delete & sync backend

--- server/src/services/note.ts
+++ server/src/services/note.ts
@@ -1,23 +1,86 @@
 import { Request, Response, Router } from "express";
 import { validationResult } from "express-validator";
 import { Note } from "../models/Note";
-import { postNoteValidation } from "../validations/note";
+import { postNoteValidation, postSyncNoteValidation, putNoteValidation } from "../validations/note";
 
-export const noteRoutes = Router().get('/', async (req, res) => {
-    const notes = await Note.find().sort([['date', -1]]);
-    res.json({status: 'ok', data: notes})
-}).post('/', postNoteValidation, async (req: Request, res: Response) => {
-    const errors = validationResult(req)
+export const noteRoutes = Router()
+
+  .get("/", async (req, res) => {
+    const notes = await Note.find().sort([["date", -1]]);
+    res.json({ status: "ok", data: notes });
+  })
+
+  .post("/", postNoteValidation, async (req: Request, res: Response) => {
+    const errors = validationResult(req);
     if (!errors.isEmpty()) {
-        return res.json({status: 'error', errors: errors.array()}).status(400)
+      return res.json({ status: "error", errors: errors.array() }).status(400);
     }
     const note = new Note({
+      description: req.body.description,
+      category: req.body.category,
+      date: req.body.date,
+      nominal: req.body.nominal,
+      createdAt: new Date(),
+    });
+    await note.save();
+    return res.json({ status: "ok", data: note });
+  })
+  
+  .put('/:id', putNoteValidation, async (req: Request, res: Response) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({status: 'error', errors: errors.array()})
+    }
+    try {
+      const note = await Note.findByIdAndUpdate(req.params.id, {
         description: req.body.description,
-        category: req.body.category,
-        date: req.body.date,
         nominal: req.body.nominal,
-        createdAt: new Date()
-    })
-    await note.save()
-    return res.json({status: 'ok', data: note})
-})
+        date: req.body.date,
+        category: req.body.category,
+        updatedAt: new Date()
+      }, {new: true});
+      return res.json({status: 'ok', data: note})
+    } catch (e) {
+      return res.status(400).json({status: 'error', errors: [{msg: 'Record not found'}]})
+    }
+  })
+  
+  .delete('/:id', async (req: Request, res: Response) => {
+    try {
+      const note = await Note.findByIdAndDelete(req.params.id);
+      if (!note) {
+        return res.status(400).json({status: 'error', errors: note})
+      }
+      return res.json({status: 'ok', data: note})
+    } catch (e) {
+      return res.status(500).json({status: 'error', errors: [{msg: 'Record not found'}]})
+    }
+  })
+  
+  .post('/sync', postSyncNoteValidation, async (req: Request, res: Response) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+      return res.status(400).json({status: 'error', errors: errors.array()})
+    }
+    try {
+      for (const data of req.body.data) {
+        const newNote = {
+          description: data.description,
+          nominal: data.nominal,
+          date: data.date,
+          category: data.category
+        }
+        if (data.status == 'insert') {
+          await Note.create(newNote)
+        } else if (data.status == 'update') {
+          await Note.findByIdAndUpdate(data._id, newNote)
+        } else if (data.status == 'delete') {
+          await Note.findByIdAndDelete(data._id)
+        }
+      }
+    } catch (e) {
+      return res.status(500).json({status: 'error', errors: [{msg: e}]})
+    }
+    const notes = await Note.find().sort([['date', -1]])
+    return res.json({status: 'ok', data: notes})
+  });
--- server/src/validations/note.ts
+++ server/src/validations/note.ts
@@ -12,3 +12,37 @@ export const postNoteValidation = [
   body("date").notEmpty().withMessage("date is required").toDate(),
   body("nominal").notEmpty().withMessage("nominal is required").toInt(),
 ];
+
+export const putNoteValidation = [
+  body("description")
+    .notEmpty()
+    .withMessage("description is required")
+    .isString()
+    .withMessage("description should be string"),
+  body("category")
+    .custom((input) => ["income", "expense"].includes(input))
+    .withMessage("category sould be income or expense"),
+  body("date").notEmpty().withMessage("date is required").toDate(),
+  body("nominal").notEmpty().withMessage("nominal is required").toInt(),
+];
+data: [
+  {
+    description: "",
+  },
+];
+export const postSyncNoteValidation = [
+  body("data.*._id").notEmpty().withMessage("id is required"),
+  body("data.*.description")
+    .notEmpty()
+    .withMessage("description is required")
+    .isString()
+    .withMessage("description should be string"),
+  body("data.*.category")
+    .custom((input) => ["income", "expense"].includes(input))
+    .withMessage("category sould be income or expense"),
+  body("data.*.date").notEmpty().withMessage("date is required").toDate(),
+  body("data.*.nominal").notEmpty().withMessage("nominal is required").toInt(),
+  body("data.*.status")
+    .custom((input) => ["insert", "update", "delete"].includes(input))
+    .withMessage("status should be insert, update, or delete"),
+];
